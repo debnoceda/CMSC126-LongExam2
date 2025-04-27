8bac933d968782df45c29c2a5d0e01b3a47b3aee
Dave Noceda <deibeunoceda26@gmail.com>
2025-04-27 16:46:22 +0800

    Edit NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,9 @@
 function NotFound(){
-    return <div>NotFound</div>
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you are looking for doesn't exist.</p>
+        <a href="/home">Go to Home</a>
+    </div>
 }
 
 export default NotFound;
