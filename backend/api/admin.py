26f68979818e39435353a0f6c191b4ea6a5cd0b7
Dave Noceda <deibeunoceda26@gmail.com>
2025-04-28 23:25:16 +0800

    Add UserProfile to admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Wallet, Transaction
+from .models import Category, Wallet, Transaction, UserProfile
 
 # Register your models with the Django admin interface
 admin.site.register(Category)
 admin.site.register(Wallet)
 admin.site.register(Transaction)
+admin.site.register(UserProfile)
