e928da6215a8c9660dc276035d4666836c495a9c
Dave Noceda <deibeunoceda26@gmail.com>
2025-04-28 23:24:15 +0800

    Create a UserProfile Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    monthly_budget = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"{self.user.email}'s profile"
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
