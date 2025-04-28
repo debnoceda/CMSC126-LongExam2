e6d9bbf389746ce98bcb26e35fb9859cdaa7a603
Dave Noceda <deibeunoceda26@gmail.com>
2025-04-28 22:43:01 +0800

    Add transaction, category, and wallet model to admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Wallet, Transaction
 
-# Register your models here.
+# Register your models with the Django admin interface
+admin.site.register(Category)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
