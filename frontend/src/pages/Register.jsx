89bd91fb812a1b78be8a2d5b1d7b9d3e275439aa
Dave Noceda <deibeunoceda26@gmail.com>
2025-05-01 14:59:17 +0800

    Add styles to Register Page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,16 @@
 import Form from "../components/Form"
+import FroggyCheerCoin from "../assets/FroggyCheerCoin.png"
+import "../styles/Register.css"
 
 function Register(){
-    return <Form route="api/users/" method="register" />
+    return <div className="register-page">
+        <div className ="register-form-container">
+            <h1>Create your Account</h1>
+            <Form route="api/users/" method="register" />
+            <p>Already have an Account? <a href="/login">Login</a></p>
+        </div>
+        <img src={FroggyCheerCoin} alt="Froggy Cheer Coin"></img>
+    </div>;
 }
 
 export default Register;
