5e6a5138c9529e32fdc4bd622668d518d637139d
rrrrawrrrr <aanapoles@up.edu.ph>
2025-05-01 09:24:38 -0700

    Added Error404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,26 @@
-function NotFound(){
-    return <div>
-        <h1>404 Not Found</h1>
-        <p>The page you are looking for doesn't exist.</p>
-        <a href="/home">Go to Home</a>
-    </div>
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
+import '../styles/NotFound.css';
+import ErrorImage from '../assets/Error404.png';
+
+function NotFound() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="notfound-container">
+            <img src={ErrorImage} alt="404 Not Found" className="notfound-image" />
+            <h2>The page you are looking for doesn't exist.</h2>
+            <div className="notfound-button-wrapper">
+                <Button
+                    type="small"
+                    text="Go to Home"
+                    onClick={() => navigate('/home')}
+                    className="bg-main-color white-color"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default NotFound;
