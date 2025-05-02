f2182ea2ba5b99b2c2d0393a713a89c97035ba6b
Dave Noceda <deibeunoceda26@gmail.com>
2025-05-02 10:36:51 +0800

    Remove user registration specific url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,6 @@ class RegisterView(generics.CreateAPIView):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', UserViewSet.as_view({'post': 'create'}), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('api-auth/', include('rest_framework.urls')),
