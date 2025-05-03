82ffc3e3639f394312ce5335c713fac051d57797
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-05-03 22:35:03 +0800

    Set balance summary from backend

--- frontend/src/components/BalanceSummary.jsx
+++ frontend/src/components/BalanceSummary.jsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 import '../styles/BalanceSummary.css'; // Import your CSS file for styling
 import FroggyImage from '../assets/Froggy.png';
+import api from '../api'; // Import your API instance
 
 function BalanceSummary() {
+  const [balanceSummary, setBalanceSummary] = useState({
+    total_balance: 0,
+    total_income: 0,
+    total_expense: 0,
+  });
+
+  useEffect(() => {
+    const fetchBalanceSummary = async () => {
+      try {
+        const response = await api.get('/api/transactions/total_balance/');
+        setBalanceSummary(response.data);
+      } catch (err) {
+        console.error('Failed to fetch balance summary:', err);
+        setError('Failed to load balance summary.');
+      } finally {
+      }
+    };
+
+    fetchBalanceSummary();
+  }, []);
+
   return (
     <Card className="bg-main-color white-color balance-summary">
-        <div>
-            <p>Balance</p>
-            <h1 className='balance'>$100</h1>
+      <div>
+        <p>Balance</p>
+        <h1 className='balance'>${balanceSummary.total_balance.toFixed(2)}</h1>
+      </div>
+      <div className="stats">
+        <div className="stats-item">
+          <p>Expenses</p>
+          <p className='bold'>${balanceSummary.total_expense.toFixed(2)}</p> 
         </div>
-        <div className="stats">
-            <div className="stats-item">
-                <p>Expenses</p>
-                <p className='bold'>$100</p> 
-            </div>
-            <div className="stats-item">
-                <p>Income</p>
-                <p className='bold'>$100</p>
-            </div>
+        <div className="stats-item">
+          <p>Income</p>
+          <p className='bold'>${balanceSummary.total_income.toFixed(2)}</p>
         </div>
-        <img src={FroggyImage} className="overlay-image" alt="Froggy" />
+      </div>
+      <img src={FroggyImage} className="overlay-image" alt="Froggy" />
     </Card>
   );
 }
