ccc51f866ee8931c465e8d4240aef5fe5f96fd49
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-05-03 18:41:24 +0800

    Wrap app with user provider context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
 import './styles/index.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -8,7 +9,6 @@ import Analytics from './pages/Analytics';
 import Transaction from './pages/Transaction';
 import Profile from './pages/Profile';
 import LandingPage from './pages/LandingPage';
-import Setup from './pages/Setup';
 import NotFound from './pages/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
 import Wallets from './pages/Wallets';
@@ -36,25 +36,26 @@ function RegisterAndLogout() {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="/logout" element={<Logout />} />
-        
-        {/* Protected Routes with Layout */}
-        <Route path="/" element={<ProtectedRoute><Layout><Navigate to="/home" /></Layout></ProtectedRoute>} />
-        <Route path="/setup" element={<ProtectedRoute><Layout><Setup /></Layout></ProtectedRoute>} />
-        <Route path="/home" element={<ProtectedRoute><Layout><Home /></Layout></ProtectedRoute>} />
-        <Route path="/wallets" element={<ProtectedRoute><Layout><Wallets/></Layout></ProtectedRoute>} />
-        <Route path="/transaction" element={<ProtectedRoute><Layout><Transaction/></Layout></ProtectedRoute>} />
-        <Route path="/analytics" element={<ProtectedRoute><Layout><Analytics /></Layout></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>} />
-        
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="/logout" element={<Logout />} />
+          
+          {/* Protected Routes with Layout */}
+          <Route path="/" element={<ProtectedRoute><Layout><Navigate to="/home" /></Layout></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><Layout><Home /></Layout></ProtectedRoute>} />
+          <Route path="/wallets" element={<ProtectedRoute><Layout><Wallets/></Layout></ProtectedRoute>} />
+          <Route path="/transaction" element={<ProtectedRoute><Layout><Transaction/></Layout></ProtectedRoute>} />
+          <Route path="/analytics" element={<ProtectedRoute><Layout><Analytics /></Layout></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>} />
+          
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
