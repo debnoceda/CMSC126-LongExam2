1ef9ce219c5b2fdc65cdbddef7cf750c16d46430
Dave Noceda <deibeunoceda26@gmail.com>
2025-05-04 21:33:33 +0800

    Prevent infinity navigation loop

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,69 +2,76 @@ import { Navigate } from "react-router-dom";
 import { jwtDecode} from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
-import { use, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import Setup from './Setup';
 
 function ProtectedRoute({ children }) {
     const { user, loading, error, fetchUserData } = useContext(UserContext);
     const [isAuthorized, setIsAuthorized] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        auth().catch(() => setIsAuthorized(false));
-    }, [])
+        const initializeAuth = async () => {
+            await auth();
+            setIsLoading(false);
+        };
+        initializeAuth();
+    }, []);
 
     useEffect(() => {
-        fetchUserData();
-    }
-    , []);
+        if (isAuthorized) {
+            fetchUserData();
+        }
+    }, [isAuthorized]);
 
     const refreshToken = async () => {
-        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
-        try{
-            const res = await api.post('/api/token/refresh', { refresh: refreshToken, });
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const res = await api.post('/api/token/refresh', { refresh: refreshToken });
             if (res.status === 200) {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access)
-                setIsAuthorized(true)
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuthorized(true);
             } else {
-                setIsAuthorized(false)
+                setIsAuthorized(false);
             }
-        }catch (error) {
-            console.log(error)
-            setIsAuthorized(false)
+        } catch (error) {
+            console.log(error);
+            setIsAuthorized(false);
         }
-
-    }
+    };
 
     const auth = async () => {
-        const token = localStorage.getItem(ACCESS_TOKEN)
+        const token = localStorage.getItem(ACCESS_TOKEN);
         if (!token) {
-            setIsAuthorized(false)
-            return
+            setIsAuthorized(false);
+            return;
         }
-        const decodedToken = jwtDecode(token)
-        const tokenExpiration = decodedToken.exp
-        const now = Date.now() / 1000
+        const decodedToken = jwtDecode(token);
+        const tokenExpiration = decodedToken.exp;
+        const now = Date.now() / 1000;
 
         if (tokenExpiration < now) {
-            await refreshToken()
+            await refreshToken();
+        } else {
+            setIsAuthorized(true);
         }
-        else {
-            setIsAuthorized(true)
-        }
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
     }
 
-    // if (loading || isAuthorized === null) {
-    //     return <div>Loading...</div>;
-    // }
-    
-    const requiresSetup = !user?.monthly_budget;
+    if (!isAuthorized) {
+        return <Navigate to="/" />;
+    }
 
-    if (isAuthorized && requiresSetup) {
+    const requiresSetup = !user?.monthly_budget;
+    if (requiresSetup) {
         return <Setup />;
     }
 
-    return isAuthorized ? children : <Navigate to="/" />;
+    return children;
 }
 
 export default ProtectedRoute;
